0ad1538780e24c3655f54a1c9ffec59935905437
rcalimlim <rossc@ifit.com>
2021-09-08 00:40:32 -0500

    add footer

--- .eleventy.js
+++ .eleventy.js
@@ -1,3 +1,5 @@
+const { format, formatRFC7231 } = require('date-fns');
+
 module.exports = function (eleventyConfig) {
   eleventyConfig.addCollection("navPages", function(collectionApi) {
     return collectionApi.getFilteredByTag("navPages").sort((a, b) => a.data.navIndex - b.data.navIndex);
@@ -5,6 +7,9 @@ module.exports = function (eleventyConfig) {
 
   eleventyConfig.addPassthroughCopy("src/styles.css");
   eleventyConfig.addPassthroughCopy("src/favicon.ico");
+
+  eleventyConfig.addFilter("formatRFC7231", (ts) => formatRFC7231(new Date(ts)));
+  eleventyConfig.addFilter("formatDate", (ts, formatString = "y LLLL dd HH:mm") => format(new Date(ts), formatString));
 
   return {
     dir: {
