25472fe495ed32e290a4c602f22cd466d544317c
Ross Calimlim <ross.calimlim@gmail.com>
2023-06-28 19:52:56 -0400

    fix date and first bullet

--- src/cv/resumeBuilder.ts
+++ src/cv/resumeBuilder.ts
@@ -53,9 +53,9 @@ const META = new ExperienceData(
   "Remote/San Francisco, CA"
 )
   .setStart(new Date(2022, 4))
-  .setEnd(new Date(2023, 5))
+  .setEnd(new Date(2023, 4))
   .addBulletPoint(
-    "Leading the migration of critical components of FB Messaging to a high-performance message sync framework, enabling end-to-end encryption, platform interoperability, and privacy compliance"
+    "Lead the migration of critical components of FB Messaging to a high-performance message sync framework, enabling end-to-end encryption, platform interoperability, and privacy compliance"
   )
   .addBulletPoint(
     "Built a new FB Messaging feature for group member reporting, providing parity with Messenger and supporting a high-priority wellbeing initiative"
